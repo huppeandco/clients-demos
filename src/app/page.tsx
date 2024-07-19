34817f2bf79323bddf5350e0bb5e0ad42e9568a3
ahmed jaber <ahmedjaber1852@gmail.com>
2024-07-19 14:11:08 +0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Canvas } from "@react-three/fiber";
 
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { useState } from "react";
+
+const scenes: any = ['forest', 'apartment', 'city', 'dawn', 'lobby']
 export default function Home() {
+  const [index, setIndex] = useState(0)
+  const [scene, setScene] = useState<any>(scenes[index]);
+
+  const gltf = useLoader(GLTFLoader, "/sahmpo.glb");
+  const handleChangeScene = () => {
+    console.log('inte')
+    if (index === 4) {
+      setIndex(0)
+      setScene(scenes[index]);
+    } else {
+      setIndex(index + 1)
+      setScene(scenes[index]);
+    }
+  }
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <Canvas style={{ background: 'transparent', height: '100vh', width: '100vw' }}>
+        <OrbitControls />
+        <Environment preset={scene} background={true} />
+        <mesh
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          castShadow
+          position={[0, -2, 0]}
+          receiveShadow
+          scale={1} // Set the initial tilt of the object
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <primitive object={gltf.scene} />
+        </mesh>
+      </Canvas>
+      <button
+      style={{position: 'fixed', cursor: 'pointer', zIndex: 10, bottom: 20, right: 20, background: 'transparent', border: '1px solid #fff' , color: '#fff', padding: 10}}
+        onClick={handleChangeScene}
+      >change scene</button>
     </main>
   );
 }
